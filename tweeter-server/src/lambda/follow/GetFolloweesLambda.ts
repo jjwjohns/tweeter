b9133063a3b1f8766df30031fae2e544d2135322
jjwjohns <jordanwjohns@gmail.com>
2025-11-13 03:38:44 -0700

    added lots of stuff

---
+++ tweeter-server/src/lambda/follow/GetFolloweesLambda.ts
@@ -0,0 +1,23 @@
+import { PagedUserItemRequest } from "tweeter-shared";
+import { PagedUserItemResponse } from "tweeter-shared";
+import { FollowService } from "../../model/service/FollowService";
+
+export const handler = async (
+  request: PagedUserItemRequest
+): Promise<PagedUserItemResponse> => {
+  const followService = new FollowService();
+
+  const [items, hasMore] = await followService.loadMoreFollowees(
+    request.token,
+    request.userAlias,
+    request.pageSize,
+    request.lastItem || null
+  );
+
+  return {
+    success: true,
+    message: null,
+    items: items,
+    hasMore: hasMore,
+  };
+};
